f1163eac832180e228ebd89eb69170a2663facd0
Olumide Ogundele <olumideralph@gmail.com>
2020-03-18 16:49:00 +0100

    create the homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Fragment } from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <Fragment>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Ticket Tracker App</h1>
+        <br/>
+        <p>Signup | Login</p>
       </header>
-    </div>
+    </Fragment>
   );
 }
 
